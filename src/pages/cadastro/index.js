ed5541c29cf5b14608ac84393900461fc6b2a4d5
Priscila Dias <62040835+PriscilaDias2003@users.noreply.github.com>
2023-09-09 19:04:42 -0300

    Update index.js

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Menu from '../../components/Menu';
 import Footer from '../../components/Footer';
 import iconPerson from '../../assets/icon-person.png';
@@ -39,6 +40,7 @@ export default function Cadastro(){
                             
                             <input type="password" id="senha" name="senha" placeholder="Senha" required />
                         </div>
+                        <p>Já possui conta? Faça <Link to="/"><strong>Login</strong></Link></p>
                     </div>
                     
                     <button type="submit" class="btn-cadastrar">Cadastrar</button>
