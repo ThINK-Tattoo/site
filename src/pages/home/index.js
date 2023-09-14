e3394dc02effbf6bc584b71866b908b4a5c149a7
koralinee <karoline.silva184@etec.sp.gov.br>
2023-09-14 11:31:36 -0300

    Confeccção da home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Menu from '../../components/MenuLog';
+import Menu from '../../components/MenuHomeLog';
 import Footer from '../../components/Footer';
 
 import '../../styleGlobal.css';
@@ -9,20 +9,35 @@ import imagemPrincipal from '../../assets/image 1.png';
 
 export default function Home(){
     return (
-        <div>
-            <Menu/>
+        <div className="containerHome">
+            
             <div className="containerPrincipal">
-                <div>
-                    <img src={imagemPrincipal} className="imagemPrincipal"></img>
-                </div>
+                <div id="imagemPrincipal">
+                <Menu/>
                 <div className="itensContainer">
                     <h1>ThINK</h1>
                     <p>Uma nova maneira de Pensar e Tatuar</p>
                     <div className="btn-agendeJa">
                         <button className="btn">Agende já!</button>
                     </div> 
-                </div>  
+                </div> 
+                    {/*<img src={imagemPrincipal} className="imagemPrincipal"></img>*/}
+                </div>
             </div>
+            <div className="grid">
+                <div class="gridContainer">
+                    <div className="gridColuna1">
+                    <span>Realismo</span>
+                    </div>
+                    <div className="gridColuna2">
+                    <span>Dotwork</span>
+                    </div>
+                    <div className="gridColuna3">
+                    <span>Fine Line</span>
+                    </div>
+                </div>
+            </div>
+
             <Footer/>
         </div>
       );
