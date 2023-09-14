0756b0c50e69a21ff3f6be95ac995347d7dfcbe7
giovannapfs <giovanna.figueirasilva@gmail.com>
2023-09-13 23:15:25 -0300

    home em construção

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,28 @@
 import React from "react";
-import Menu from '../../components/Menu';
+import Menu from '../../components/MenuLog';
 import Footer from '../../components/Footer';
 
 import '../../styleGlobal.css';
 import './index.css'
 
+import imagemPrincipal from '../../assets/image 1.png';
+
 export default function Home(){
     return (
         <div>
             <Menu/>
-            
+            <div className="containerPrincipal">
+                <div>
+                    <img src={imagemPrincipal} className="imagemPrincipal"></img>
+                </div>
+                <div className="itensContainer">
+                    <h1>ThINK</h1>
+                    <p>Uma nova maneira de Pensar e Tatuar</p>
+                    <div className="btn-agendeJa">
+                        <button className="btn">Agende já!</button>
+                    </div> 
+                </div>  
+            </div>
             <Footer/>
         </div>
       );
