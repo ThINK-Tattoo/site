98dc370dc777574902c00faaa2c77e5d69c5a8ba
giovannapfs <giovanna.figueirasilva@gmail.com>
2023-09-24 14:39:54 -0300

    pagina principal do dashboard

--- src/pages/admin/homeDashboard/index.js
+++ src/pages/admin/homeDashboard/index.js
@@ -4,13 +4,20 @@ import Menu from '../../../components/admin/menuDashboard';
 import '../../../styleGlobal.css';
 import './index.css'
 
+import imagem from '../../../assets/dashboardHome/home.png'
+
 export default function homeDashboard(){
     return (
         <div>
             <Menu />
+            <section>
             <div className="tituloDashboard">
                 <h1>Página Inic<span className="span-color-dashboard">ial</span></h1>
             </div>
+            <div className="imagem">
+                <img src={imagem} className="homeDash"></img>
+            </div>
+            </section>
         </div>
         
     )
