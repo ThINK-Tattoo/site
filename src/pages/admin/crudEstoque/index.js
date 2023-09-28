2a74ac826695575ab14b48ccba73d552cd999b21
giovannapfs <giovanna.figueirasilva@gmail.com>
2023-09-28 15:18:48 -0300

    algumas alterações

--- src/pages/admin/crudEstoque/index.js
+++ src/pages/admin/crudEstoque/index.js
@@ -4,16 +4,13 @@ import Menu from '../../../components/admin/menuDashboard';
 import '../../../styleGlobal.css';
 import './index.css'
 
-export default function crudEstoque(){
+export default function CrudEstoque(){
     return (
         <div>
             <Menu/>
             <section>
             <div className="tituloDashboard">
                 <h1>Esto<span className="span-color-dashboard">que</span></h1>
-            </div>
-            <div>
-                
             </div>
             </section>
         </div>
