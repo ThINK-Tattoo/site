b8227a3cef32504d08ebec2cd00650f31f113381
giovannapfs <giovanna.figueirasilva@gmail.com>
2023-11-21 21:41:49 -0300

    adicionando modal do token

--- src/pages/cliente/cadastro/index.js
+++ src/pages/cliente/cadastro/index.js
@@ -4,6 +4,10 @@ import Menu from '../../../components/visitante/MenuVisitante';
 import MenuLogado from "../../../components/usuarioLogado/MenuLog";
 import Footer from '../../../components/Footer';
 import { hotjar } from "react-hotjar";
+import MaskedInput from 'react-input-mask';
+import logo from "../../../assets/icones/logo-removebg-preview 1.png"
+
+import Modal from 'react-modal';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
@@ -23,12 +27,64 @@ export default function Cadastro(){
         setIsUserLoggedIn(userType === "cliente");
     }, []);
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const openModal = (e) => {
+        e.preventDefault();
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    
     return (
         <div className="container container-cadastro">
             {isUserLoggedIn ? <MenuLogado /> : <Menu />}
             <div className="header-image cadastro-tittle">
                 <h1>Cadas<span className="span-color">tro</span></h1>
             </div>
+            <Modal
+                isOpen={isModalOpen}
+                onRequestClose={closeModal}
+                id="modal-cadastro"
+                contentLabel="Autenticação de dois fatores"
+                style={{
+                    overlay: {
+                        backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+                    },
+                    content: {
+                        top: '50%', 
+                        left: '50%', 
+                        transform: 'translate(-50%, -50%)', 
+                        backgroundColor: '#000',
+                        height: '80%'
+                    },
+                }}
+            >
+                <form>
+                    <img src={logo} className="logoModal"/>
+                    <h2>Autenticação de dois fatores</h2>
+                    <div className="token">
+                        <div className="tokenInside">
+                            <input type="text" maxLength={1} minLength={1} style={{textAlign: 'center', fontSize: '20px', textTransform: 'uppercase'}}></input>
+                            <input type="text" maxLength={1} minLength={1} style={{textAlign: 'center', fontSize: '20px', textTransform: 'uppercase'}}></input>
+                            <input type="text" maxLength={1} minLength={1} style={{textAlign: 'center', fontSize: '20px', textTransform: 'uppercase'}}></input>
+                        </div>
+                        <div className="tokenInside">
+                            <input type="text" maxLength={1} minLength={1} style={{textAlign: 'center', fontSize: '20px', textTransform: 'uppercase'}}></input>
+                            <input type="text" maxLength={1} minLength={1} style={{textAlign: 'center', fontSize: '20px', textTransform: 'uppercase'}}></input>
+                            <input type="text" maxLength={1} minLength={1} style={{textAlign: 'center', fontSize: '20px', textTransform: 'uppercase'}}></input>
+                        </div>
+                    </div>
+                    <p>Uma mensagem com um código de verificação foi enviada para seu e-mail. Insira o código para continuar.</p>
+                    <div className="reenvio">
+                        <h4>Não recebeu um código de verificação?</h4>
+                        <h4 id="reenvio2" >Enviar novamente</h4>
+                    </div>
+                </form>
+            </Modal>
+
             <section className="form-container">
                 <form class="form cadastro">
                 <FontAwesomeIcon icon={faUser} id="person-icon" />
@@ -51,7 +107,7 @@ export default function Cadastro(){
                         </div>
                         <p>Já possui conta? Faça <Link to="/signin"><strong>Login</strong></Link></p>
                     </div>
-                    <button type="submit" class="btn btn-cadastrar">Cadastrar</button>
+                    <button type="submit" class="btn btn-cadastrar" onClick={(e) => openModal(e)}>Cadastrar</button>
                 </form>
             </section>
             <Footer/>
